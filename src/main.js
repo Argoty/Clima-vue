a1fb95269f09e12653b525e146c4cb022b1da006
JaxisArty <jaxis0810@gmail.com>
2023-01-09 10:56:20 -0500

    clima hecho

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: function (h) { return h(App) },
+  vuetify,
+  render: function (h) { return h(App) }
 }).$mount('#app')
